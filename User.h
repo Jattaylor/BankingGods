1848add0aeea8244a92f31fb61bcd2ad3669a06f
Jordan Taylor <46736136+Jattaylor@users.noreply.github.com>
2019-11-14 16:16:19 -0500

    Added User.h

--- User.h
+++ User.h
@@ -1,1 +1,25 @@
 //User.h
+#include<string>
+using namespace std;
+#ifndef USER_H
+#define USER_H
+class User{
+public:
+
+User();
+User(string,string,string,string,string,string);
+void setName(string, string);
+void setDOB(string,string,string);
+void setID(string);
+virtual void print();
+
+private:
+string first_name;
+string last_name;
+string day;
+string month;
+string year;
+string id;
+
+};
+#endif
